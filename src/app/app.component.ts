721657213feb5a95b3d0b6a87344089157d1b756
thuan274 <doanminhthuan274@gmail.com>
2017-11-17 22:24:08 +0700

    add utility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 // tslint:disable-next-line:prefer-const
 import * as franc from "franc";
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,7 +21,17 @@ export class AppComponent implements OnInit {
   }
 
   checkText() {
-    console.log(franc.all(this.text, {whitelist: ['eng', 'vie', 'jpn']}));
-    this.text = '';
+    // console.log(franc.all(this.text, {whitelist: ['eng', 'vie', 'jpn']}));
+    let splits = _.split(this.text, '\n');
+    _.forEach(splits, text => {
+      this.checkIsEnglish(text);
+    });
+    // this.text = '';
+  }
+
+
+  checkIsEnglish(text: string) {
+    console.log(text);
+    console.log(franc.all(text, {whitelist: ['eng', 'vie', 'jpn']}));
   }
 }
---
+++ src/app/utility/utility-check.spec.ts
@@ -0,0 +1,26 @@
+import { checkSystem, checkCanDelete } from "./utility-check";
+
+describe('Test with script system', () => {
+  it('should return true', () => {
+    let text = '@立ち絵 id="始皇帝", time=300';
+    expect(checkSystem(text)).toBeTruthy();
+  });
+});
+
+fdescribe('Test can delete', () => {
+  it('should first test case', () => {
+    let text = "Don't worry&.";
+    expect(checkCanDelete(text)).toBeTruthy();
+  });
+
+  it("should 2nd test case", () => {
+    let text = "<FONT interval=60>＊Cough＊&.&.&. Làm phiền&.&.&. mọi người rồi&.&.&. ＊cough cough＊&.&.&.</FONT>";
+    expect(checkCanDelete(text)).toBeFalsy();
+  });
+
+  it("should 3rd test case", () => {
+    let text = '-message id="始皇帝", voice="始皇帝##C10_始皇帝_01_お薬を求めて_本#0008.ogg"';
+    expect(checkCanDelete(text)).toBeFalsy();
+  });
+
+});
